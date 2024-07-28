496535572509695600542670ab80f6489ef63c2d
DannyFabs <aderemifawoyed@gmail.com>
2024-07-28 02:51:36 -0500

    atp

--- api/chat.js
+++ api/chat.js
@@ -8,7 +8,7 @@ const port = 3001
 const cors = require('cors');
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5501/'); // Allow requests from your frontend
+    res.header('Access-Control-Allow-Origin', '*'); // Allow requests from your frontend
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE'); // Allow these methods
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow these headers
     next();

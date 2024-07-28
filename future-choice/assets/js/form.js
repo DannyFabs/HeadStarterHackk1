e1967cdd4d9dde71b6006b42b562b1772a202244
DannyFabs <aderemifawoyed@gmail.com>
2024-07-28 04:09:30 -0500

    hh

--- future-choice/assets/js/form.js
+++ future-choice/assets/js/form.js
@@ -62,60 +62,60 @@ function validateForm() {
 document.getElementById("submit").addEventListener("click", async function(event) {
     event.preventDefault(); // Prevent the default form submission
 
-    if (validateForm()) {
-        // Create an object to store the results
-        const user_data = {
-            1: getRadioValue('web_development'),
-            2: getRadioValue('database_management'),
-            3: getRadioValue('data_structures_and_algorithms'),
-            4: getRadioValue('systems_design'),
-            5: getRadioValue('software_development_practices'),
-            6: getRadioValue('networking_and_security'),
-            7: getRadioValue('operating_sys'),
-            8: getRadioValue('mobile_dev'),
-            9: getRadioValue('problem_solving'),
-            10: getRadioValue('critical_thinking'),
-            11: getRadioValue('communication'),
-            12: getRadioValue('teamwork'),
-            13: getRadioValue('leadership'),
-            14: getRadioValue('creativity'),
-            15: getRadioValue('adaptability'),
-            16: getRadioValue('attention_to_detail'),
-            17: getRadioValue('work_environment'),
-            18: getRadioValue('learning_style'),
-            19: getRadioValue('work_style'),
-            20: getRadioValue('problem_solving_approach'),
-            21: getRadioValue('certifications'),
-            22: getRadioValue('job_value')
-        };
+    // if (validateForm()) {
+    //     // Create an object to store the results
+    //     const user_data = {
+    //         1: getRadioValue('web_development'),
+    //         2: getRadioValue('database_management'),
+    //         3: getRadioValue('data_structures_and_algorithms'),
+    //         4: getRadioValue('systems_design'),
+    //         5: getRadioValue('software_development_practices'),
+    //         6: getRadioValue('networking_and_security'),
+    //         7: getRadioValue('operating_sys'),
+    //         8: getRadioValue('mobile_dev'),
+    //         9: getRadioValue('problem_solving'),
+    //         10: getRadioValue('critical_thinking'),
+    //         11: getRadioValue('communication'),
+    //         12: getRadioValue('teamwork'),
+    //         13: getRadioValue('leadership'),
+    //         14: getRadioValue('creativity'),
+    //         15: getRadioValue('adaptability'),
+    //         16: getRadioValue('attention_to_detail'),
+    //         17: getRadioValue('work_environment'),
+    //         18: getRadioValue('learning_style'),
+    //         19: getRadioValue('work_style'),
+    //         20: getRadioValue('problem_solving_approach'),
+    //         21: getRadioValue('certifications'),
+    //         22: getRadioValue('job_value')
+    //     };
 
         // Log the results to the console (optional)
         // console.log(formResults);
 
 
-        msg_content = `Based on the following inputs by a new cs graduate give top 2 cs jobs that would be a good fit in 20 words or less:
-        Web Development:Rating:${user_data[1]}
-        Database Management:Rating:${user_data[2]} 
-        Data Structures and Algorithms:Rating:${user_data[3]} 
-        Systems Design:Rating:${user_data[4]} 
-        Software Development Practices:Rating:${user_data[5]} 
-        Networking and Security:Rating:${user_data[6]} 
-        Operating Systems::Rating:${user_data[7]} 
-        Mobile Development::Rating:${user_data[8]} 
-        Machine Learning and Data Science:Rating:${user_data[9]} 
-        Problem Solving:Rating:${user_data[10]} 
-        Communication:Rating:${user_data[11]}
-        Creativity and design:Rating:${user_data[12]}
-        Leadership:Rating:${user_data[13]}
-        Time management:Rating:${user_data[14]} 
-        Analytical thinking:Rating:${user_data[15]} 
-        Adaptability:Rating:${user_data[16]} 
-        How do you prefer to work?Answer:${user_data[17]}
-        How do you prefer to learn new skills?Answer:${user_data[18]}
-        What type of work environment do you prefer?Answer:${user_data[19]}
-        How do you approach problem-solving?Answer:${user_data[20]}
-        Are you interested in obtaining additional certifications or pursuing a graduate degree?${user_data[21]}
-        What do you value most in a job?Answer:${user_data[22]}`
+        // msg_content = `Based on the following inputs by a new cs graduate give top 2 cs jobs that would be a good fit in 20 words or less:
+        // Web Development:Rating:${user_data[1]}
+        // Database Management:Rating:${user_data[2]} 
+        // Data Structures and Algorithms:Rating:${user_data[3]} 
+        // Systems Design:Rating:${user_data[4]} 
+        // Software Development Practices:Rating:${user_data[5]} 
+        // Networking and Security:Rating:${user_data[6]} 
+        // Operating Systems::Rating:${user_data[7]} 
+        // Mobile Development::Rating:${user_data[8]} 
+        // Machine Learning and Data Science:Rating:${user_data[9]} 
+        // Problem Solving:Rating:${user_data[10]} 
+        // Communication:Rating:${user_data[11]}
+        // Creativity and design:Rating:${user_data[12]}
+        // Leadership:Rating:${user_data[13]}
+        // Time management:Rating:${user_data[14]} 
+        // Analytical thinking:Rating:${user_data[15]} 
+        // Adaptability:Rating:${user_data[16]} 
+        // How do you prefer to work?Answer:${user_data[17]}
+        // How do you prefer to learn new skills?Answer:${user_data[18]}
+        // What type of work environment do you prefer?Answer:${user_data[19]}
+        // How do you approach problem-solving?Answer:${user_data[20]}
+        // Are you interested in obtaining additional certifications or pursuing a graduate degree?${user_data[21]}
+        // What do you value most in a job?Answer:${user_data[22]}`
 
         console.log(msg_content);
 
@@ -123,6 +123,6 @@ document.getElementById("submit").addEventListener("click", async function(event
         const careerSoln =  await sendData(msg_content);
         localStorage.setItem('careerSolution', JSON.stringify(careerSoln));
         window.location.href = 'Result.html';
-    }
+    // }
 });
 

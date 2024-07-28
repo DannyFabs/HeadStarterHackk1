d81cf893a1f9117375ff2a877b4c34659393a14f
cammartinezx <64925363+cammartinezx@users.noreply.github.com>
2024-07-27 22:42:27 -0400

    Add files via upload

---
+++ future-choice/assets/js/form.js
@@ -0,0 +1,45 @@
+function getRadioValue(name) {
+    const radios = document.getElementsByName(name);
+    for (const radio of radios) {
+        if (radio.checked) {
+            return radio.value;
+        }
+    }
+    return null; // Return null if no radio button is checked
+}
+
+document.getElementById("submit").addEventListener("click", function(event) {
+    console.log("jolljds");
+    event.preventDefault(); // Prevent the default form submission
+
+    // Create an object to store the results
+    const formResults = {
+        1: getRadioValue('web_development'),
+        2: getRadioValue('database_management'),
+        3: getRadioValue('data_structures_and_algorithms'),
+        4: getRadioValue('systems_design'),
+        5: getRadioValue('software_development_practices'),
+        6: getRadioValue('networking_and_security'),
+        7: getRadioValue('operating_sys'),
+        8: getRadioValue('mobile_dev'),
+        9: getRadioValue('problem_solving'),
+        10: getRadioValue('critical_thinking'),
+        11: getRadioValue('communication'),
+        12: getRadioValue('teamwork'),
+        13: getRadioValue('leadership'),
+        14: getRadioValue('creativity'),
+        15: getRadioValue('adaptability'),
+        16: getRadioValue('attention_to_detail'),
+        17: getRadioValue('work_environment'),
+        18: getRadioValue('learning_style'),
+        19: getRadioValue('work_style'),
+        20: getRadioValue('problem_solving_approach'),
+        21: getRadioValue('certifications'),
+        22: getRadioValue('job_value')
+    };
+
+    // Log the results to the console (optional)
+    console.log(formResults);
+    window.location.href = 'index.html';
+
+});

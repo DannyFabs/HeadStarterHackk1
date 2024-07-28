31987aa94ed924d5cccb995cb12234c886cf6f38
DannyFabs <aderemifawoyed@gmail.com>
2024-07-28 01:58:48 -0500

    cors about to take me out

--- future-choice/assets/js/form.js
+++ future-choice/assets/js/form.js
@@ -61,33 +61,34 @@ function validateForm() {
 document.getElementById("submit").addEventListener("click", async function(event) {
     event.preventDefault(); // Prevent the default form submission
 
-    if (validateForm()) {
+    // if (validateForm()) {
         // Create an object to store the results
-        const formResults = {
-            1: getRadioValue('web_development'),
-            2: getRadioValue('database_management'),
-            3: getRadioValue('data_structures_and_algorithms'),
-            4: getRadioValue('systems_design'),
-            5: getRadioValue('software_development_practices'),
-            6: getRadioValue('networking_and_security'),
-            7: getRadioValue('operating_sys'),
-            8: getRadioValue('mobile_dev'),
-            9: getRadioValue('problem_solving'),
-            10: getRadioValue('critical_thinking'),
-            11: getRadioValue('communication'),
-            12: getRadioValue('teamwork'),
-            13: getRadioValue('leadership'),
-            14: getRadioValue('creativity'),
-            15: getRadioValue('adaptability'),
-            16: getRadioValue('attention_to_detail'),
-            17: getRadioValue('work_environment'),
-            18: getRadioValue('learning_style'),
-            19: getRadioValue('work_style'),
-            20: getRadioValue('problem_solving_approach'),
-            21: getRadioValue('certifications'),
-            22: getRadioValue('job_value')
-        };
+        // const formResults = {
+        //     1: getRadioValue('web_development'),
+        //     2: getRadioValue('database_management'),
+        //     3: getRadioValue('data_structures_and_algorithms'),
+        //     4: getRadioValue('systems_design'),
+        //     5: getRadioValue('software_development_practices'),
+        //     6: getRadioValue('networking_and_security'),
+        //     7: getRadioValue('operating_sys'),
+        //     8: getRadioValue('mobile_dev'),
+        //     9: getRadioValue('problem_solving'),
+        //     10: getRadioValue('critical_thinking'),
+        //     11: getRadioValue('communication'),
+        //     12: getRadioValue('teamwork'),
+        //     13: getRadioValue('leadership'),
+        //     14: getRadioValue('creativity'),
+        //     15: getRadioValue('adaptability'),
+        //     16: getRadioValue('attention_to_detail'),
+        //     17: getRadioValue('work_environment'),
+        //     18: getRadioValue('learning_style'),
+        //     19: getRadioValue('work_style'),
+        //     20: getRadioValue('problem_solving_approach'),
+        //     21: getRadioValue('certifications'),
+        //     22: getRadioValue('job_value')
+        // };
 
+        const formResults = {}
         // Log the results to the console (optional)
         console.log(formResults);
 
@@ -95,6 +96,6 @@ document.getElementById("submit").addEventListener("click", async function(event
         const careerSoln =  await sendData(formResults);
         localStorage.setItem('careerSolution', JSON.stringify(careerSoln));
         window.location.href = 'Result.html';
-    }
+    // }
 });
 
